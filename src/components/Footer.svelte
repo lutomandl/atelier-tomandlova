175f16cda388b76f02121edb007b5afb8dbe0fec
lubost <lubos.tomandl@citrix.com>
2021-03-22 22:01:52 +0100

    add responsivity to Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,58 +4,73 @@ import Kontakt from "../routes/kontakt.svelte";
 
 </script>
 <style>
-    footer {
-        
-        width: 100vw;
+    section.light {
         display: flex;
         justify-content: center;
-        padding: 70px 0;
-        position: relative;
     }
 
-    section.light {
+    div.container {
         display: grid;
         grid-template-columns: 200px 200px;
         column-gap: 20px;
-        z-index: 2;
     }
 
     div.column {
         display: flex;
         flex-direction: column;
+        text-align: left;
+        overflow:visible;
     }
 
-    img {
+    img.logo {
         width: 150px;
     }
 
     img.small-circle {
-        width: 15vw;
+        width: 300px;
         position: absolute;
         top: -30px;
-        left: 35vw;
     }
 
     p {
         margin-top: 0;
+    }
+
+    @media (max-width: 450px) {
+        div.container {
+            display: flex;
+            flex-direction: column;
+        }
+
+        img.logo {
+            margin-bottom: 24px;
+        }
+
+        img.small-circle{
+            width: 220px;
+            top: -40px;
+            left: 30px;
+        }
     }
 </style>
 
 
 <footer>
     <section class="light">
-        <div class="column">
-            <img alt="at-logo" src="logo.png">
-        </div>
-        <div class="column">
-            <p>Ateliér Tomandlová<br>
-                Židovská 9<br>
-                Cheb
-            </p>
-            <a href="mailto:info@ateliertomandlova.cz"><p>info@ateliertomandlova.cz</p></a>
-            <p>Po - Pá:&ensp;9 - 16 hod</p>
-            <a href="kontakt"><button>Kontaktujte nás</button></a>
+        <div class="container">
+            <div class="column">
+                <img class="logo" alt="at-logo" src="logo.png">
+                <img class="graphics small-circle" alt="circle" src="transparent-small-circle.svg">
+            </div>
+            <div class="column">
+                <p>Ateliér Tomandlová<br>
+                    Židovská 9<br>
+                    Cheb
+                </p>
+                <a href="mailto:info@ateliertomandlova.cz"><p>info@ateliertomandlova.cz</p></a>
+                <p>Po - Pá:&ensp;9 - 16 hod</p>
+                <a href="kontakt"><button>Kontaktujte nás</button></a>
+            </div>
         </div>
     </section>
-    <img class="small-circle" alt="circle" src="transparent-small-circle.svg">
 </footer>
