a7364d72add2e98206d7fe16f81e055e1515f7dd
lubost <lubos.tomandl@citrix.com>
2021-05-30 09:55:52 +0200

    aktuality update

--- src/routes/aktuality/[slug].svelte
+++ src/routes/aktuality/[slug].svelte
@@ -21,6 +21,22 @@ import Index from "../index.svelte";
 </script>
 
 <style>
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		padding-left: 150px;
+	}
+	
+	h1 {
+		margin-top: 70px;
+		margin-bottom: 0;
+	}
+
+	div.content {
+		margin-top: 12px;
+	}
+
 	/*
 		By default, CSS is locally scoped to the component,
 		and any unused styles are dead-code-eliminated.
@@ -55,12 +71,41 @@ import Index from "../index.svelte";
 		margin: 0 0 0.5em 0;
 	} */
 
-	section {
+	div.galery {
 		display: flex;
-		flex-direction: column;
-		text-align: left;
+		flex-wrap: wrap;
+		width: 100%;
+		justify-content: center;
+		margin: 40px 0;
 	}
-	
+
+	div.photo{
+		width: 210px;
+		height: 297px;
+		border-radius: 2px;
+		margin: 24px 10px;
+		overflow: hidden;
+        border-bottom: 1px solid transparent;
+	}
+
+	div.poster {
+		width: 315px !important; 
+		height: 441px !important;
+	}
+
+    div.photo:hover {
+        cursor: pointer;
+        box-shadow: 0.5px 0.5px 15px 0px #00000033;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: top center;
+        border-radius: 0;
+        transition: 300ms;
+    }
 </style>
 
 <svelte:head>
@@ -74,10 +119,18 @@ import Index from "../index.svelte";
 		{@html post.html}
 	</div>
 	{#if post.poster}
-		<embed type="pdf" src="photos/{post.poster}" width="800px" height="2100px" />
+		<div class="photo poster">
+			<img class="poster" alt="poster" src="photos/{post.poster}">
+		</div>
 	{/if}
 	{#if post.images.length > 0}
-		<!-- image galery -->
+		<div class="galery">
+			{#each post.images as image}
+				<div class="photo">
+					<img alt={image} src="photos/{image}">
+				</div>
+			{/each}
+		</div>
 	{/if}
 </section>
 	
