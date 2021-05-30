869a84204e0026be33954375805ae02aa2524fef
lubost <lubos.tomandl@citrix.com>
2021-05-30 10:16:23 +0200

    update opening hours

--- src/components/OpeningHours.svelte
+++ src/components/OpeningHours.svelte
@@ -9,24 +9,24 @@
 <table>
     <tr>
         <td>Po</td>
-        <td>10:30 - 13:00</td>
+        <td>10:30 - 12:00</td>
         <td>a</td>
-        <td>14:00 - 17:00</td>
+        <td>13:00 - 17:00</td>
     </tr>
     <tr>
         <td>Út - Čt</td>
-        <td>10:00 - 13:00</td>
+        <td>10:00 - 12:00</td>
         <td>a</td>
-        <td>14:00 - 17:00</td>
+        <td>13:00 - 17:00</td>
     </tr>
     <tr>
         <td>Pá</td>
-        <td>10:00 - 13:00</td>
+        <td>10:00 - 12:00</td>
         <td>a</td>
-        <td>14:00 - 16:00</td>
+        <td>13:00 - 16:00</td>
     </tr>
     <tr>
         <td>So</td>
-        <td>po předchozí dohodě</td>
+        <td>po předchozí domluvě</td>
     </tr>
 </table>
