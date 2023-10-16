176604b8732dd7fdfd2338b299e894e7245f96a0
Lubos Tomandl <lubos.tomandl@netcentric.biz>
2023-10-16 21:26:31 +0200

    add background graphic paralax element

---
+++ src/components/BackgroundGraphic.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let y: number;
+  export let paralaxSpeed: number = 4;
+  export let graphic: string;
+  export let top: number | undefined = undefined;
+  export let left: number | undefined = undefined;
+  export let right: number | undefined = undefined;
+  export let width: number;
+</script>
+
+<div
+  class="backgroundGraphic"
+  style={`transform: translateY(-${y / paralaxSpeed}px);
+    top: ${top}%; 
+    left: ${left}%; 
+    right: ${right}%;
+    width: ${width}%;
+  `}
+>
+  <img src={graphic} alt="background graphic element" />
+</div>
+
+<style lang="scss">
+  .backgroundGraphic {
+    position: absolute;
+
+    & > img {
+      width: 100%;
+      height: auto;
+    }
+  }
+</style>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,10 +3,21 @@
   import arrow from '$lib/assets/arrow.svg';
   import Typography from './Typography.svelte';
   import translations from '../utils/useTranslations';
+  import BackgroundGraphic from './BackgroundGraphic.svelte';
+  import yellowLine from '$lib/assets/yellow-line.svg';
+  import smallCircle from '$lib/assets/transparent-small-circle.svg';
+  import circle from '$lib/assets/circle.svg';
+
+  let y: number;
 </script>
+
+<svelte:window bind:scrollY={y} />
 
 <header class="header">
   <Section>
+    <BackgroundGraphic graphic={yellowLine} {y} top={40} left={20} width={30} />
+    <BackgroundGraphic graphic={smallCircle} {y} paralaxSpeed={5} top={5} right={5} width={15} />
+    <BackgroundGraphic graphic={circle} {y} paralaxSpeed={2} top={20} right={20} width={30} />
     <div class="header__title">
       <Typography variant="header" element="h1">{translations.mainPage.title}</Typography>
     </div>
