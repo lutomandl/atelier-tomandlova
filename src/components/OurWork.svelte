117a8c236cb00ac9bada34a128c84cfb131420de
Lubos Tomandl <lubos.tomandl@netcentric.biz>
2023-10-21 11:44:00 +0200

    ourwork and typography mobil optimization

--- src/components/OurWork.svelte
+++ src/components/OurWork.svelte
@@ -7,6 +7,7 @@
   import fabric from '$lib/assets/fabric.svg';
   import Button from './Button.svelte';
   import translations from '../utils/useTranslations';
+  import BackgroundGraphic from './BackgroundGraphic.svelte';
 
   onMount(() => {
     console.info('Icons made by https://www.freepik.com, downloaded from https://www.flaticon.com');
@@ -14,26 +15,34 @@
 </script>
 
 <Section bg="light">
+  <BackgroundGraphic graphic="hill" top={50} left={60} width={30} />
+  <BackgroundGraphic graphic="splash" top={80} left={10} width={40} paralaxSpeed={2} />
   <div id="our-work" class="ourWork">
     <Typography variant="h1" element="h2">{translations.mainPage.ourWork}</Typography>
     <div class="ourWork__columns">
       <div class="ourWork__column">
         <img alt="design icon" src={design} />
-        <Typography variant="h2" element="h3">{translations.mainPage.designHeading}</Typography>
+        <div class="ourWork__column__title">
+          <Typography variant="h2" element="h3">{translations.mainPage.designHeading}</Typography>
+        </div>
         <Typography>
           {translations.mainPage.designText}
         </Typography>
       </div>
       <div class="ourWork__column">
         <img alt="fabric icon" src={fabric} />
-        <Typography variant="h2" element="h3">{translations.mainPage.qualityHeading}</Typography>
+        <div class="ourWork__column__title">
+          <Typography variant="h2" element="h3">{translations.mainPage.qualityHeading}</Typography>
+        </div>
         <Typography>
           {translations.mainPage.qualityText}
         </Typography>
       </div>
       <div class="ourWork__column">
         <img alt="sewing machine icon" src={sewingMachine} />
-        <Typography variant="h2" element="h3">{translations.mainPage.handmadeHeading}</Typography>
+        <div class="ourWork__column__title">
+          <Typography variant="h2" element="h3">{translations.mainPage.handmadeHeading}</Typography>
+        </div>
         <Typography>{translations.mainPage.handmadeText}</Typography>
       </div>
     </div>
